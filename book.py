68c6141e9285db6300d6849e60d29a307cb77f40
harikiran <neeliharikiran@gmail.com>
2025-02-17 22:43:31 +0530

    [harikiarn] Add:python script for printing welcome message

--- book.py
+++ book.py
@@ -1,1 +1,25 @@
-print("hello welcome to address book system")
+class Address_Book_Main:
+    def display(self):
+
+        '''
+        Description:To print the welcome to address book message
+        Parameter:None
+        Return:None
+        '''
+
+        print("Welcome to address book")
+
+def main():
+
+    """
+    Description: To create an instance of Address_Book_Main class and 
+    print the welcome message using the display method.
+    Parameter: None
+    Return: None
+    """
+    obj = Address_Book_Main()
+    obj.display()
+
+
+if __name__ == '__main__' :
+    main()
