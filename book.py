30fd90e2d4cff2a153914e4b54d569fe5025eb5d
harikiran <neeliharikiran@gmail.com>
2025-02-18 11:48:46 +0530

    [harikiran] Add: python script to collect and display the user details

--- book.py
+++ book.py
@@ -1,25 +1,36 @@
-class Address_Book_Main:
-    def display(self):
+def user_details():
 
-        '''
-        Description:To print the welcome to address book message
-        Parameter:None
-        Return:None
-        '''
+    """
+    Description: Collects user details like name, address, city, state, zip, phone number, and email.
+    Parameter: None
+    Return: Dictionary containing user details.
+    """
 
-        print("Welcome to address book")
+    user_info = {
+        "First Name": input("Enter the first name: "),
+        "Last Name": input("Enter the last name: "),
+        "Address": input("Enter the address: "),
+        "City": input("Enter the city: "),
+        "State": input("Enter the state: "),
+        "Zip": input("Enter the city zip: "),
+        "Phone Number": int(input("Enter the phone number: ")),
+        "Email": input("Enter the email: ")
+    }
+    return user_info
 
 def main():
 
     """
-    Description: To create an instance of Address_Book_Main class and 
-    print the welcome message using the display method.
+    Description:prints the user details 
     Parameter: None
     Return: None
     """
-    obj = Address_Book_Main()
-    obj.display()
+
+    user_info = user_details()
+    print("\nUser Details:")
+    for key, value in user_info.items():
+        print(f"{key}: {value}")
 
 
-if __name__ == '__main__' :
+if __name__ == '__main__':
     main()
