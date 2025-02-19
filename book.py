d386c1cb61db27d2b1d0d4e74b86385e842c3aaa
harikiran <neeliharikiran@gmail.com>
2025-02-19 15:35:56 +0530

    [harikiran]Add:python script to add multiple address and also implemented in loggers

--- book.py
+++ book.py
@@ -1,3 +1,45 @@
+from loggers import logger
+
+
+class Add_multiple:
+
+    def __init__(self,address_book):
+
+        '''
+        Description:To in create a new empty list and new instance to add the the multiple address in to the main list in one push
+        parameter:address_book
+        return:None
+        '''
+
+        logger.info("initilizing Add_ multiple class")
+        self.multiple_contacts=[]
+        self.Valide=address_book
+        
+    def add_contact(self):
+
+        '''
+        Description:collecting the multiple address and add them  to the main list
+        parameter:None
+        return:None 
+        '''
+
+        logger.info("adding multiple contacts")
+        no_of_contacts = int(input("ente the how many contacts you need to enter : "))
+        for i in range (no_of_contacts):
+            first_name=input("enter the first name : ")
+            last_name = input("enter the last name : ")
+            address = input("enter the address : ")
+            zip = self.Valide.validate_zip()
+            phone_number=self.Valide.validate_phone()
+            mail=input("enter the mail : ")
+            contact ={'first_name': first_name,'last_name': last_name,'address': address,'zip':zip,'phone_number':phone_number,'mail':mail }
+            self.multiple_contacts.append(contact)
+            logger.info(f"address added:{contact}")
+            print("\n address added successfully ")
+        self.Valide.contacts.extend(self.multiple_contacts)
+        logger.info("seccess fully multiple address to the main list")
+        print("\n successfully added to the main list")
+   
 class address_book_info:
 
     def __init__(self):
@@ -7,10 +49,11 @@ class address_book_info:
         parameters:None
         return:None
         '''
-
+        logger.info("intilizing address_book_info")
         self.contacts =[]
     
     def validate_zip(self):
+
 
         '''
         Description:To check the formate of zip
@@ -21,8 +64,10 @@ class address_book_info:
         while True:
             zip=input("enter the zip : ")
             if zip.isdigit():
+                logger.info(f"valid zip:{zip}")
                 return zip
             else:
+                logger.warning("invalid zip")
                 print("error: the input should be in integer")
                 continue
                 
@@ -37,8 +82,10 @@ class address_book_info:
         while True:
             phone =input("enter the phone number : ")  
             if phone.isdigit() and len(phone) == 10:
+                logger.info(f"valid zip : {phone}")
                 return phone
             else:
+                logger.warning("invalid phone number")
                 print("error the phone number should be in integer and atlest 10 ")   
             
     def add_contact(self):
@@ -58,6 +105,7 @@ class address_book_info:
         
         contact = {'first_name': first_name,'last_name': last_name,'address': address,'zip':zip,'phone_number':phone_number,'mail':mail }
         self.contacts.append(contact)
+        logger.info(f"address added:{contact}")
         print("\n added contact successfully")
 
     def display(self):
@@ -69,8 +117,10 @@ class address_book_info:
         '''
 
         if not self.contacts:
+            logger.info("no contact in the address book")
             print("no people in the address book")
         else:
+            logger.info("displaying contacts")
             print("address book contacts")
             for i in self.contacts:
                 print(i)
@@ -88,6 +138,7 @@ class address_book_info:
             last = input("Enter the last name you want to edit: ")
             for i in range(len(self.contacts)):
                 if self.contacts[i]['first_name'] == first and self.contacts[i]['last_name'] == last:
+                    logger.info(f"editing contact info of {self.contacts[i]}")
                     print("\nContact found. You can edit the details now.")
                     while True:
                         print("\nAvailable keys: first_name, last_name, address, zip, phone_number, mail")
@@ -97,6 +148,7 @@ class address_book_info:
                             return
                         elif key in self.contacts[i]:
                             if key == 'zip':
+                                logger.info("editing completed")
                                 new_value = self.validate_zip()
                             elif key == 'phone_number':
                                 new_value = self.validate_phone()
@@ -104,12 +156,15 @@ class address_book_info:
                                 new_value = input(f"Enter new value for {key}: ")
 
                             self.contacts[i][key] = new_value
+                            logger.info(f"updated {key}:{new_value}")
                             print(f"{key} updated successfully.")
 
                         else:
+                            logger.warning(f"invalid {key}")
                             print("Invalid key. Try again.")
                             continue
             else:
+               logger.info("contact not found for editing")
                print("Contact not found Try again")
                continue
     
@@ -126,10 +181,12 @@ class address_book_info:
             last=input("enter the last name to delete : ")
             for i in range (len(self.contacts)):
                 if self.contacts[i]['first_name'] == first and self.contacts[i]['last_name']==last:
+                    logger.info(f"deletin contact:{self.contacts[i]}")
                     del self.contacts[i]
                     print("\nsuccessfully deleted the address")
                     return
             else:
+               logger.warning("contact not found to delete")
                print("contact not found  try again")
                continue
 
@@ -143,6 +200,7 @@ class Address_book:
         return:None
         '''
 
+        logger.info("initializing Address_book class")
         self.address_book = address_book_info()
     
     def main(self):
@@ -159,18 +217,28 @@ class Address_book:
             print("3 to exit contact")
             print("4 to edit existing user")
             print("5 to delete the address")
+            print("6 to add multiple address")
             value= input(" \nenter the value : ")
             if value == '1':
+                logger.info("user chosen to add the address")
                 self.address_book.add_contact()
             elif value == '2':
+                logger.info("user chosen to dispaly the address")
                 self.address_book.display()
             elif value == '3':
+                logger.info("user chosen to exit the program")
                 print("exiting program..")
                 break
             elif value == '4':
+                logger.info("user chosen to edit the address")
                 self.address_book.edit_details()
             elif value == '5':
+                logger.info("user chosen to delete the address")
                 self.address_book.delete_address()
+            elif value == '6':
+                logger.info("user chosen to add multiple address")
+                self.add_multiple=Add_multiple(self.address_book)
+                self.add_multiple.add_contact()
             else:
                 print("invalid number")
 
